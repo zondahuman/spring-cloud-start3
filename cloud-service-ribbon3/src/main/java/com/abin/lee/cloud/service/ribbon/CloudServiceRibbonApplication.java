52dd5d922dec95999fa83082cefc732e131b01a9
zondahuman <zondahuman@gmail.com>
2018-09-28 22:56:13 +0800

    add

--- cloud-service-ribbon3/src/main/java/com/abin/lee/cloud/service/ribbon/CloudServiceRibbonApplication.java
+++ cloud-service-ribbon3/src/main/java/com/abin/lee/cloud/service/ribbon/CloudServiceRibbonApplication.java
@@ -1,11 +1,19 @@
 package com.abin.lee.cloud.service.ribbon;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 /**
  * Created by abin on 2018/9/26.
  */
+@EnableDiscoveryClient
+@SpringBootApplication
 public class CloudServiceRibbonApplication {
 
-
-
+    public static void main(String[] args) {
+        SpringApplication.run(CloudServiceRibbonApplication.class, args);
+//        new SpringApplicationBuilder(CloudServiceRibbonApplication.class).web(true).run(args);
+    }
 
 }
