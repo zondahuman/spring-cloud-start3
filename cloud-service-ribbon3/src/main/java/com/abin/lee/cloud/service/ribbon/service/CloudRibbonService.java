04fdb83f552d9a300862f11a503354f6e04e387b
zondahuman <zondahuman@gmail.com>
2018-09-29 16:47:49 +0800

    add

--- cloud-service-ribbon3/src/main/java/com/abin/lee/cloud/service/ribbon/service/CloudRibbonService.java
+++ cloud-service-ribbon3/src/main/java/com/abin/lee/cloud/service/ribbon/service/CloudRibbonService.java
@@ -4,6 +4,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.HashMap;
@@ -21,17 +23,17 @@ public class CloudRibbonService {
 
 
     public Integer add(Integer param1, Integer param2) {
-        Map<String, Object> request = new HashMap<>();
-        request.put("param1", param1);
-        request.put("param2", param2);
+        MultiValueMap<String, Object> request = new LinkedMultiValueMap<>();
+        request.add("param1", param1);
+        request.add("param2", param2);
         Integer result = restTemplate.postForObject("http://cloud-service-provider/add", request, Integer.class);
         return result;
     }
 
     public Map<String, String> find(String name, String count) {
-        Map<String, Object> request = new HashMap<>();
-        request.put("name", name);
-        request.put("count", count);
+        MultiValueMap<String, Object> request = new LinkedMultiValueMap<>();
+        request.add("name", name);
+        request.add("count", count);
 
         Map<String, String> response = restTemplate.postForObject("http://cloud-service-provider/find", request, Map.class);
 
